977d0c7cf42189c323e1e017ca6304c1c8dbe100
xdiex <28797462+xdiex@users.noreply.github.com>
2023-08-15 11:03:33 +0900

    typoの修正

--- backend/keycloak/my-numbercard-authenticator/src/main/java/com/example/mynumbercardidp/keycloak/core/network/platform/PlatformApiClientLoaderImpl.java
+++ backend/keycloak/my-numbercard-authenticator/src/main/java/com/example/mynumbercardidp/keycloak/core/network/platform/PlatformApiClientLoaderImpl.java
@@ -24,12 +24,12 @@ interface PlatformApiClientLoaderImpl {
     /**
      * クラスのFQDNからプラットフォームと通信するためのクラスインスタンスを返します。
      *
-     * プラットフォームAPIクライアントを初期化するときにIdp送信者符号も渡します。
+     * プラットフォームAPIクライアントを初期化するときにIdP送信者符号も渡します。
      *
      * @param platformClassFqdn プラットフォームAPIクライアントの完全修飾クラス名
      * @param context 認証フローのコンテキスト
      * @param apiRootUri プラットフォームAPIのルートURI（処理による変更が無い共通URI部分）
-     * @param idpSender プラットフォームへ送るIdp送信者の識別符号
+     * @param idpSender プラットフォームへ送るIdP送信者の識別符号
      * @return プラットフォームAPIクライアントインスタンス
      */
     PlatformApiClientImpl load(String platformClassFqdn, AuthenticationFlowContext context, String apiRootUri, String idpSender);
