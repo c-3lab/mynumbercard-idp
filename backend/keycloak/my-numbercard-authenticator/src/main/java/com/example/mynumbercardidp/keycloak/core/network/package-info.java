766424438ba8ac3ba5b246eee80042ab04454fc5
xdiex <28797462+xdiex@users.noreply.github.com>
2023-08-15 11:03:33 +0900

    パッケージの説明を追加

---
+++ backend/keycloak/my-numbercard-authenticator/src/main/java/com/example/mynumbercardidp/keycloak/core/network/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 個人番号カードの公的個人認証部分を受け付ける認証SPIが必要とする通信機能及びユーザーのリクエストモデルを作成するために必要なクラスを定義します。
+ */
+package com.example.mynumbercardidp.keycloak.core.network;
---
+++ backend/keycloak/my-numbercard-authenticator/src/main/java/com/example/mynumbercardidp/keycloak/util/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * ユーティリティクラスを定義します。機能に依存しないクラスはこのパッケージに直接配置します。
+ */
+package com.example.mynumbercardidp.keycloak.util;
