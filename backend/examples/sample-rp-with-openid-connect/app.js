1d81259b6ac9461670d0d851b3d7219a2711bc40
Sheile <sheile1024@gmail.com>
2023-08-15 20:46:39 +0900

    Add /me route with requiring authentication

--- backend/examples/sample-rp-with-openid-connect/app.js
+++ backend/examples/sample-rp-with-openid-connect/app.js
@@ -1,4 +1,4 @@
-const { auth } = require('express-openid-connect')
+const { auth, requiresAuth } = require('express-openid-connect')
 const express = require('express')
 const app = express()
 
@@ -22,10 +22,14 @@ app.use(auth(config))
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
   if (req.oidc.isAuthenticated()) {
-    res.send(`<a href="/logout">Logout</a><div>${JSON.stringify(req.oidc.user)}</div>`)
+    res.send(`<a href="/logout">Logout</a><div>${JSON.stringify(req.oidc.user)}</div><a href="/me">/me (require login)</a>`)
   } else {
-    res.send('<a href="/login">Login</a>')
+    res.send('<a href="/login">Login</a><br><a href="/me">/me (require login)</a>')
   }
+})
+
+app.get('/me', requiresAuth(), (req, res) => {
+  res.send(`<h1>me</h1><div>${JSON.stringify(req.oidc.user)}</div>`)
 })
 
 app.listen(3000, () => {
