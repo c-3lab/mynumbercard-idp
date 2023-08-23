0ce702a7abe890cc9b7f3a30f402acba554841ca
mizuno <y-mizuno@abelsoft.co.jp>
2023-08-23 17:40:27 +0900

    不要なNSLocalizedStringを削除

--- iOS/MyNumberCardAuth/MyNumberCardAuth/Models/HTTPSession.swift
+++ iOS/MyNumberCardAuth/MyNumberCardAuth/Models/HTTPSession.swift
@@ -49,8 +49,8 @@ public class HTTPSession: NSObject, URLSessionDelegate, URLSessionTaskDelegate{
                    httpResponse.statusCode == 503){
                     DispatchQueue.main.async {
                         self.authenticationController.isAlert = true
-                        self.authenticationController.messageTitle = NSLocalizedString("Failure", comment: "失敗")
-                        self.authenticationController.messageString = NSLocalizedString("An unexpected error has occurred.", comment: "予期せぬエラーが発生しました。")
+                        self.authenticationController.messageTitle = String(localized:"Failure", comment: "失敗")
+                        self.authenticationController.messageString = String(localized:"An unexpected error has occurred.", comment: "予期せぬエラーが発生しました。")
                     }
                 }
                 
@@ -58,19 +58,19 @@ public class HTTPSession: NSObject, URLSessionDelegate, URLSessionTaskDelegate{
                     DispatchQueue.main.async {
                         self.authenticationController.isAlert = true
                         self.authenticationController.isErrorOpenURL = true
-                        self.authenticationController.messageTitle = NSLocalizedString("Authentication failure", comment: "認証失敗")
-                        self.authenticationController.messageString = NSLocalizedString("Since you have not registered as a user, please register as a user.", comment: "ユーザー未登録のため、利用者登録を行ってください。")
+                        self.authenticationController.messageTitle = String(localized:"Authentication failure", comment: "認証失敗")
+                        self.authenticationController.messageString = String(localized:"Since you have not registered as a user, please register as a user.", comment: "ユーザー未登録のため、利用者登録を行ってください。")
                     }
                 }
 
                 if(httpResponse.statusCode == 401){
                     DispatchQueue.main.async {
                         self.authenticationController.isLinkAlert = true
-                        self.authenticationController.messageTitle = NSLocalizedString("Authentication failure", comment: "認証失敗")
+                        self.authenticationController.messageTitle = String(localized:"Authentication failure", comment: "認証失敗")
                         if(self.authenticationController.runMode == .Login){
-                            self.authenticationController.messageString = NSLocalizedString("The electronic certificate for user certification has been revoked.", comment: "利用者証明用電子証明書が失効しています。") + NSLocalizedString("Please contact the window of your municipality, or contact the window of the My Number Card Comprehensive Site from the link below.", comment: "お住まいの市区町村の窓口へお問い合わせください。")
+                            self.authenticationController.messageString = String(localized:"The electronic certificate for user certification has been revoked.", comment: "利用者証明用電子証明書が失効しています。") + String(localized:"Please contact the window of your municipality, or contact the window of the My Number Card Comprehensive Site from the link below.", comment: "お住まいの市区町村の窓口へお問い合わせください。")
                         }else{
-                            self.authenticationController.messageString = NSLocalizedString("The electronic signature certificate has expired.", comment: "署名用電子証明書が失効しています。") + NSLocalizedString("Please contact the window of your municipality, or contact the window of the My Number Card Comprehensive Site from the link below.", comment: "お住まいの市区町村の窓口へお問い合わせください。")
+                            self.authenticationController.messageString = String(localized:"The electronic signature certificate has expired.", comment: "署名用電子証明書が失効しています。") + String(localized:"Please contact the window of your municipality, or contact the window of the My Number Card Comprehensive Site from the link below.", comment: "お住まいの市区町村の窓口へお問い合わせください。")
                         }
                     }
                 }
@@ -80,8 +80,8 @@ public class HTTPSession: NSObject, URLSessionDelegate, URLSessionTaskDelegate{
                     DispatchQueue.main.async {
                         self.authenticationController.isAlert = true
                         self.authenticationController.isErrorOpenURL = true
-                        self.authenticationController.messageTitle = NSLocalizedString("Registration failed", comment: "登録失敗")
-                        self.authenticationController.messageString = NSLocalizedString("Since the user is already registered, please log in.", comment: "既にユーザーが登録されているため、ログインを行ってください。")
+                        self.authenticationController.messageTitle = String(localized:"Registration failed", comment: "登録失敗")
+                        self.authenticationController.messageString = String(localized:"Since the user is already registered, please log in.", comment: "既にユーザーが登録されているため、ログインを行ってください。")
                     }
                 }
                 
@@ -96,8 +96,8 @@ public class HTTPSession: NSObject, URLSessionDelegate, URLSessionTaskDelegate{
                         self.authenticationController.viewState = .SignatureView
                         self.authenticationController.runMode = .Replacement
                         self.authenticationController.isAlert = true
-                        self.authenticationController.messageTitle = NSLocalizedString("Reloading My Number Card", comment: "マイナンバーカードの再読み込み")
-                        self.authenticationController.messageString = NSLocalizedString("I need to change my registration information. Please read the signature digital certificate to make changes.", comment: "登録情報の変更が必要です。変更を行うため署名用電子証明書の読み取りを行ってください。")
+                        self.authenticationController.messageTitle = String(localized:"Reloading My Number Card", comment: "マイナンバーカードの再読み込み")
+                        self.authenticationController.messageString = String(localized:"I need to change my registration information. Please read the signature digital certificate to make changes.", comment: "登録情報の変更が必要です。変更を行うため署名用電子証明書の読み取りを行ってください。")
                     }
                 }
                 
--- iOS/MyNumberCardAuth/MyNumberCardAuth/Views/NFCReadingForSignatureView.swift
+++ iOS/MyNumberCardAuth/MyNumberCardAuth/Views/NFCReadingForSignatureView.swift
@@ -16,19 +16,19 @@ struct NFCReadingForSignatureView: View {
     
     var body: some View {
         VStack{
-            Text(NSLocalizedString("My number card \n authentication", comment: "マイナンバーカード認証"))
+            Text("My number card \n authentication")
                 .font(.title)
                 .multilineTextAlignment(.center)
                 .bold()
                 .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             
-            (Text(NSLocalizedString("Enter the password (6 to 16 alphanumeric characters) of the electronic ", comment: "マイナンバーカードの")) + Text(NSLocalizedString("certificate for signature ", comment: "署名用電子証明書")).fontWeight(.bold) + Text(NSLocalizedString("of My Number Card and press the start reading button ", comment: "のパスワード（6～16桁の英数字）を入力し、読み取り開始ボタンを押す")))
+            (Text("Enter the password (6 to 16 alphanumeric characters) of the electronic ") + Text("certificate for signature ").fontWeight(.bold) + Text("of My Number Card and press the start reading button "))
                 .font(.title3)
                 .multilineTextAlignment(.center)
                 .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                 .frame(height: 140.0)
             
-            SecureField(NSLocalizedString("password", comment: "暗証番号"), text: $controller.inputPIN)
+            SecureField("password", text: $controller.inputPIN)
                 .padding(.all)
                 .frame( height: 80.0)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -37,7 +37,7 @@ struct NFCReadingForSignatureView: View {
                 .toolbar {
                     ToolbarItemGroup(placement: .keyboard) {
                         Spacer()
-                        Button(NSLocalizedString("close", comment: "閉じる")) {
+                        Button("close") {
                             isActive = false
                         }
                     }
@@ -60,13 +60,13 @@ struct NFCReadingForSignatureView: View {
                     }))
                 }
             
-            Button(NSLocalizedString("start reading", comment: "読み取り開始")) {
+            Button("start reading") {
                 if controller.isEnableButton() {
                     self.authenticationController.startReading(pin: controller.inputPIN, nonce: controller.nonce ,actionURL: controller.actionURL)
                 }
             }.modifier(SmallButtonModifier(color: controller.getButtonColor()))
             .alert(isPresented:self.$authenticationController.isLinkAlert){
-                    Alert(title:Text(self.authenticationController.messageTitle),message:Text(self.authenticationController.messageString),primaryButton: .default(Text(NSLocalizedString("Contact page", comment: "問い合わせページ")),action: {
+                    Alert(title:Text(self.authenticationController.messageTitle),message:Text(self.authenticationController.messageString),primaryButton: .default(Text("Contact page"),action: {
                         if (self.authenticationController.inquiryURL.isEmpty == false)
                         {
                             if let openURL = URL(string:self.authenticationController.inquiryURL){
