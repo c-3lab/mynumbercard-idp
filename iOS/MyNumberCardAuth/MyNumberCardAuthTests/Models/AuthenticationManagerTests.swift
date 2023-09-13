c955c0c961e5361868b9eb2e255f168b637d1094
mizuno <y-mizuno@abelsoft.co.jp>
2023-09-13 16:50:15 +0900

    xctest,xcuitestファイルの追加

---
+++ iOS/MyNumberCardAuth/MyNumberCardAuthTests/Models/AuthenticationManagerTests.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  AuthenticationManagerTests
+//
+//  Created by c3lab on 2023/09/12.
+//
+import XCTest
+@testable import MyNumberCardAuth
+import SwiftUI
+
+final class AuthenticationManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    
+    
+    
+}
