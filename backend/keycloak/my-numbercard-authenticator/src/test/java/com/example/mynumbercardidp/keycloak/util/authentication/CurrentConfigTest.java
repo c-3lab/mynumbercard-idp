e4c27f9e1c20f85c471f90d92e55e7bd73436f71
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-09-13 05:25:41 +0000

    100%カバレッジテストを追加

---
+++ backend/keycloak/my-numbercard-authenticator/src/test/java/com/example/mynumbercardidp/keycloak/util/authentication/CurrentConfigTest.java
@@ -0,0 +1,45 @@
+package com.example.mynumbercardidp.keycloak.util.authentication;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.models.AuthenticatorConfigModel;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.example.mynumbercardidp.keycloak.authentication.authenticators.browser.SpiConfigProperty;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Map;
+
+public class CurrentConfigTest {
+
+    @Mock
+    AuthenticationFlowContext context;
+    @Mock
+    AuthenticatorConfigModel authenticatorConfig;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        Map<String, String> config = Map.of(
+            "my-num-cd-auth.platform-class", "PlatformApiClientClassFqdn",
+            "NAME", "my-num-cd-auth.platform-class",
+            "LABEL", "Platform API Client Class FQDN",
+            "HELP_TEXT", "Fully qualified class name of the platform API client for authentication using the public personal authentication information from a My Number Card.",
+            "TYPE", "String",
+            "DEFAULT_VALUE", "com.example.mynumbercardidp.keycloak.network.platform.PlatformApiClient"
+        );
+        Mockito.when(context.getAuthenticatorConfig()).thenReturn(authenticatorConfig);
+        Mockito.when(authenticatorConfig.getConfig()).thenReturn(config);
+    }
+
+    @Test
+    public void getValue() {
+        assertEquals("", CurrentConfig.getValue(context, "TestConfig"));
+        assertNotNull(CurrentConfig.getValue(context, SpiConfigProperty.PlatformApiClientClassFqdn.CONFIG.getName()));
+    }
+}
