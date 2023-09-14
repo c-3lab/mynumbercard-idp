ba69ed0bc3cec13799738335b3745a16403bc79e
KeiIshidaBKS <99153529+KeiIshidaBKS@users.noreply.github.com>
2023-09-14 21:13:31 +0900

    IndividualNumberReaderに対するモックについて、一部メソッドを実装

---
+++ iOS/MyNumberCardAuth/MyNumberCardAuthTests/Models/IndividualNumberReaderMock.swift
@@ -0,0 +1,50 @@
+//
+//  IndividualNumberReaderMock.swift
+//  MyNumberCardAuthTests
+//
+//  Created by c3lab on 2023/09/12.
+//
+
+import CoreNFC
+import TRETJapanNFCReader_MIFARE_IndividualNumber
+#if canImport(TRETJapanNFCReader_Core)
+import TRETJapanNFCReader_Core
+#endif
+#if canImport(TRETJapanNFCReader_MIFARE)
+import TRETJapanNFCReader_MIFARE
+#endif
+#if os(iOS)
+#if canImport(TRETJapanNFCReader_Core)
+import TRETJapanNFCReader_Core
+#endif
+#if canImport(TRETJapanNFCReader_MIFARE)
+import TRETJapanNFCReader_MIFARE
+#endif
+import CoreNFC
+#endif
+
+
+class IndividualNumberReaderMock: IndividualNumberReaderProtocol {
+    init() { }
+
+
+    private(set) var getCallCount = 0
+    var getHandler: (([IndividualNumberCardItem], String) -> ())?
+    func get(items: [IndividualNumberCardItem], cardInfoInputSupportAppPIN: String)  {
+        getCallCount += 1
+        if let getHandler = getHandler {
+            getHandler(items, cardInfoInputSupportAppPIN)
+        }
+        
+    }
+
+    private(set) var lookupRemainingPINCallCount = 0
+    var lookupRemainingPINHandler: ((IndividualNumberCardPINType, @escaping (Int?) -> Void) -> ())?
+    func lookupRemainingPIN(pinType: IndividualNumberCardPINType, completion: @escaping (Int?) -> Void)  {
+        lookupRemainingPINCallCount += 1
+        if let lookupRemainingPINHandler = lookupRemainingPINHandler {
+            lookupRemainingPINHandler(pinType, completion)
+        }
+    }
+}
+
