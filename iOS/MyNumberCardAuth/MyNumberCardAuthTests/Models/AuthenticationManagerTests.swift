ff4aae93238cb2524f846846bf7a8e9b6641e6ab
Ke Ishida <k-ishida@abelsoft.co.jp>
2023-09-16 18:15:44 +0900

    AuthenticationManagerに関するテストケースを追加
    - AuthenticationManagerのカバレッジが19%なので、まだ、テストケースが必要

--- iOS/MyNumberCardAuth/MyNumberCardAuthTests/Models/AuthenticationManagerTests.swift
+++ iOS/MyNumberCardAuth/MyNumberCardAuthTests/Models/AuthenticationManagerTests.swift
@@ -17,5 +17,45 @@ final class AuthenticationManagerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testAuthenticateForUserVerification() throws {}
+    func testAuthenticateForUserVerification() throws {
+        var readerMock: IndividualNumberReaderMock?
+        let manager = AuthenticationManager { _ in
+            readerMock = IndividualNumberReaderMock()
+            readerMock!.computeDigitalSignatureHandler = {
+                XCTAssertEqual($0, .userAuthentication)
+                XCTAssertEqual($1, "1234")
+                XCTAssertEqual($2, [UInt8]("0123456789".utf8))
+            }
+            return readerMock!
+        }
+
+        manager.authenticateForUserVerification(pin: "1234",
+                                                nonce: "0123456789",
+                                                actionURL: "https://example.com",
+                                                authenticationController: AuthenticationController())
+
+        XCTAssertNotNil(readerMock)
+        XCTAssertEqual(readerMock?.computeDigitalSignatureCallCount, 1)
+    }
+
+    func testAuthenticateForSignature() throws {
+        var readerMock: IndividualNumberReaderMock?
+        let manager = AuthenticationManager { _ in
+            readerMock = IndividualNumberReaderMock()
+            readerMock!.computeDigitalSignatureHandler = {
+                XCTAssertEqual($0, .digitalSignature)
+                XCTAssertEqual($1, "5678")
+                XCTAssertEqual($2, [UInt8]("67890012345".utf8))
+            }
+            return readerMock!
+        }
+
+        manager.authenticateForSignature(pin: "5678",
+                                         nonce: "67890012345",
+                                         actionURL: "https://example.com/1",
+                                         authenticationController: AuthenticationController())
+
+        XCTAssertNotNil(readerMock)
+        XCTAssertEqual(readerMock?.computeDigitalSignatureCallCount, 1)
+    }
 }
