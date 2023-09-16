2705721d4c8a124dd69512d258304bee84afe467
Ke Ishida <k-ishida@abelsoft.co.jp>
2023-09-17 05:23:54 +0900

    URLSessionDataTaskとURLSessionに対するMockを作成

---
+++ iOS/MyNumberCardAuth/MyNumberCardAuth/Protocols/URLSessionProtorol.swift
@@ -0,0 +1,24 @@
+//
+//  URLSessionProtorol.swift
+//  MyNumberCardAuth
+//
+//  Created by c3lab on 2023/09/16.
+//
+
+import Foundation
+
+/// @mockable(override: name = URLSessionMock)
+protocol URLSessionProtorol {
+    func dataTask(with request: URLRequest,
+                  completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+}
+
+extension URLSession: URLSessionProtorol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        let ret: URLSessionDataTask = self.dataTask(with: request,
+                                                    completionHandler: completionHandler)
+        return ret
+    }
+    
+
+}
