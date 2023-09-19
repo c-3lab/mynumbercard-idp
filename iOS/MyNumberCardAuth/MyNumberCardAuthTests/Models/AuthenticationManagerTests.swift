eac32f588f98c95cdba47d1f5afaf55e95eb4ecd
Ke Ishida <k-ishida@abelsoft.co.jp>
2023-09-19 13:38:20 +0900

    AuthenticationManagerのテストケースで、AuthenticationManager.swift内を実行する範囲を増やした(AuthenticationManager.swiftのカバレッジ76.0%)

--- iOS/MyNumberCardAuth/MyNumberCardAuthTests/Models/AuthenticationManagerTests.swift
+++ iOS/MyNumberCardAuth/MyNumberCardAuthTests/Models/AuthenticationManagerTests.swift
@@ -4,8 +4,10 @@
 //
 //  Created by c3lab on 2023/09/12.
 //
+
+import Foundation
 @testable import MyNumberCardAuth
-import SwiftUI
+@testable import TRETJapanNFCReader_MIFARE_IndividualNumber
 import XCTest
 
 final class AuthenticationManagerTests: XCTestCase {
@@ -19,24 +21,29 @@ final class AuthenticationManagerTests: XCTestCase {
 
     func testAuthenticateForUserVerification() throws {
         var readerMock: IndividualNumberReaderMock?
-        let manager = AuthenticationManager(makeReader: { _ in
-                                                readerMock = IndividualNumberReaderMock()
-                                                readerMock!.computeDigitalSignatureHandler = {
-                                                    XCTAssertEqual($0, .userAuthentication)
-                                                    XCTAssertEqual($1, "1234")
-                                                    XCTAssertEqual($2, [UInt8]("0123456789".utf8))
-                                                }
-                                                return readerMock!
-                                            },
-                                            makeHTTPSession: { _ in
-                                                HTTPSessionMock()
-                                            })
+        var manager: AuthenticationManager!
+        manager = AuthenticationManager(makeReader: { _ in
+                                            readerMock = IndividualNumberReaderMock()
+                                            readerMock!.computeDigitalSignatureHandler = {
+                                                XCTAssertEqual($0, .userAuthentication)
+                                                XCTAssertEqual($1, "1234")
+                                                XCTAssertEqual($2, [UInt8]("0123456789".utf8))
+                                                var cardData = TRETJapanNFCReader_MIFARE_IndividualNumber.IndividualNumberCardData()
+                                                cardData.computeDigitalSignatureForUserAuthentication = Array("5678".utf8)
+                                                cardData.userAuthenticationCertificate = Array("9012".utf8)
+                                                manager.individualNumberReaderSession(didRead: cardData)
+                                            }
+                                            return readerMock!
+                                        },
+                                        makeHTTPSession: { _ in
+                                            HTTPSessionMock()
+                                        })
         let controller = AuthenticationControllerMock()
         manager.authenticationController = controller
 
         manager.authenticateForUserVerification(pin: "1234",
                                                 nonce: "0123456789",
-                                                actionURL: "https://example.com")
+                                                actionURL: "https://example.com/realms/1")
 
         XCTAssertNotNil(readerMock)
         XCTAssertEqual(readerMock?.computeDigitalSignatureCallCount, 1)
@@ -44,24 +51,29 @@ final class AuthenticationManagerTests: XCTestCase {
 
     func testAuthenticateForSignature() throws {
         var readerMock: IndividualNumberReaderMock?
-        let manager = AuthenticationManager(makeReader: { _ in
-                                                readerMock = IndividualNumberReaderMock()
-                                                readerMock!.computeDigitalSignatureHandler = {
-                                                    XCTAssertEqual($0, .digitalSignature)
-                                                    XCTAssertEqual($1, "5678")
-                                                    XCTAssertEqual($2, [UInt8]("67890012345".utf8))
-                                                }
-                                                return readerMock!
-                                            },
-                                            makeHTTPSession: { _ in
-                                                HTTPSessionMock()
-                                            })
+        var manager: AuthenticationManager!
+        manager = AuthenticationManager(makeReader: { _ in
+                                            readerMock = IndividualNumberReaderMock()
+                                            readerMock!.computeDigitalSignatureHandler = {
+                                                XCTAssertEqual($0, .digitalSignature)
+                                                XCTAssertEqual($1, "5678")
+                                                XCTAssertEqual($2, [UInt8]("67890012345".utf8))
+                                                var cardData = TRETJapanNFCReader_MIFARE_IndividualNumber.IndividualNumberCardData()
+                                                cardData.computeDigitalSignatureForDigitalSignature = Array("5678".utf8)
+                                                cardData.digitalSignatureCertificate = Array("9012".utf8)
+                                                manager.individualNumberReaderSession(didRead: cardData)
+                                            }
+                                            return readerMock!
+                                        },
+                                        makeHTTPSession: { _ in
+                                            HTTPSessionMock()
+                                        })
         let controller = AuthenticationControllerMock()
         manager.authenticationController = controller
 
         manager.authenticateForSignature(pin: "5678",
                                          nonce: "67890012345",
-                                         actionURL: "https://example.com/1")
+                                         actionURL: "https://example.com/realms/2")
 
         XCTAssertNotNil(readerMock)
         XCTAssertEqual(readerMock?.computeDigitalSignatureCallCount, 1)
