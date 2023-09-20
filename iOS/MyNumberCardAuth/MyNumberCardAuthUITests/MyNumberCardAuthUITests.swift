59d99ef61927bd679899dbe362f49e81b30e6ef6
mizuno <y-mizuno@abelsoft.co.jp>
2023-09-20 20:51:02 +0900

    UIテストケースの作成

--- iOS/MyNumberCardAuth/MyNumberCardAuthUITests/MyNumberCardAuthUITests.swift
+++ iOS/MyNumberCardAuth/MyNumberCardAuthUITests/MyNumberCardAuthUITests.swift
@@ -7,8 +7,9 @@
 
 import XCTest
 
-// ユニバーサルリンクを使用するため、keycloakへのアクセスが可能な状態にすること
-var keycloakURL: String = "https://852ed96220eb.ngrok.app" // ポート80
+// ユニバーサルリンクを使用するため、keycloakへのアクセスが可能な状態でテストを実行してください。
+// 下記変数を使用するkeycloakのアドレスとレルムに変更してテストを行なってください。
+var keycloakURL: String = "https://54f4c17847af.ngrok.app" // ポート80
 var relum: String = "OIdp"
 
 final class MyNumberCardAuthUITests: XCTestCase {
@@ -36,6 +37,25 @@ final class MyNumberCardAuthUITests: XCTestCase {
         XCTAssertTrue(app.buttons["利用規約"].exists)
         XCTAssertTrue(app.buttons["プライバシーポリシー"].exists)
         XCTAssertTrue(app.buttons["個人情報保護方針"].exists)
+        
+        let safari = XCUIApplication(bundleIdentifier: "com.apple.mobilesafari")
+
+        app.buttons["利用規約"].tap()
+        sleep(3)
+        XCTAssertTrue(safari.staticTexts["利用規約"].exists)
+        app.activate()
+        
+        sleep(3)
+        app.buttons["プライバシーポリシー"].tap()
+        sleep(3)
+        XCTAssertTrue(safari.staticTexts["プライバシーポリシー"].exists)
+        app.activate()
+        
+        sleep(3)
+        app.buttons["個人情報保護方針"].tap()
+        sleep(3)
+        XCTAssertTrue(safari.staticTexts["個人情報保護方針"].exists)
+        app.activate()
     }
 
     func testNFCReadingForUserVerificationView() throws {
@@ -43,26 +63,35 @@ final class MyNumberCardAuthUITests: XCTestCase {
         XCUIDevice.shared.press(.home)
         XCUIApplication(bundleIdentifier: "com.apple.springboard").swipeDown()
         let spotlight = XCUIApplication(bundleIdentifier: "com.apple.Spotlight")
-        spotlight.textFields["SpotlightSearchField"].typeText(keycloakURL + "/realms/" + relum + "/login-actions/authenticate?action_url=https://example&mode=login")
+        spotlight.textFields["SpotlightSearchField"].typeText(keycloakURL + "/realms/" + relum + "/login-actions/authenticate?action_url=https://example&mode=login&nonce=1234")
         spotlight.buttons["Go"].tap()
-
+        
         XCTAssertTrue(app.staticTexts["マイナンバーカード\n認証"].exists)
         XCTAssertTrue(app.staticTexts["マイナンバーカードの利用者証明用電子証明書のパスワード（4桁の数字）を入力し、読み取り開始ボタンを押してください"].exists)
         XCTAssertTrue(app.secureTextFields.firstMatch.exists)
         XCTAssertTrue(app.buttons["読み取り開始"].exists)
+        
+        app.secureTextFields.firstMatch.tap()
+        app.secureTextFields.firstMatch.typeText("12345")
+        app.buttons["閉じる"].tap()
     }
 
     func testNFCReadingForSignatureView() throws {
         let app = XCUIApplication()
+        
         XCUIDevice.shared.press(.home)
         XCUIApplication(bundleIdentifier: "com.apple.springboard").swipeDown()
         let spotlight = XCUIApplication(bundleIdentifier: "com.apple.Spotlight")
-        spotlight.textFields["SpotlightSearchField"].typeText(keycloakURL + "/realms/" + relum + "/login-actions/authenticate?action_url=https://example&mode=registration")
+        spotlight.textFields["SpotlightSearchField"].typeText(keycloakURL + "/realms/" + relum + "/login-actions/authenticate?action_url=https://example&mode=registration&nonce=1234")
         spotlight.buttons["Go"].tap()
-
+        
         XCTAssertTrue(app.staticTexts["マイナンバーカード\n認証"].exists)
         XCTAssertTrue(app.staticTexts["マイナンバーカードの署名用電子証明書のパスワード（6～16桁の英数字）を入力し、読み取り開始ボタンを押してください"].exists)
         XCTAssertTrue(app.secureTextFields.firstMatch.exists)
         XCTAssertTrue(app.buttons["読み取り開始"].exists)
+        
+        app.secureTextFields.firstMatch.tap()
+        app.secureTextFields.firstMatch.typeText("abcdefghij01234567")
+        app.buttons["閉じる"].tap()
     }
 }
