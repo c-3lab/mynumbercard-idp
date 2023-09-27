4a70c4789cc9614b3dbb5ffecfa6445096d15bb2
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-09-27 08:20:15 +0000

    DEBUG画面用のエレメントと処理を削除

--- backend/keycloak/my-numbercard-authenticator/src/main/resources/theme/mynumbercard-auth/login/resources/js/login.js
+++ backend/keycloak/my-numbercard-authenticator/src/main/resources/theme/mynumbercard-auth/login/resources/js/login.js
@@ -10,80 +10,6 @@ function getMobileOS() {
     return 'other';
 }
 
-async function encryptDataAsJWE(data, jwksUrl) {
-    // JWKS形式の公開鍵をダウンロードして公開鍵を読み込み
-    const response = await fetch(jwksUrl);
-    const JWKS = await response.json();
-    const jwk = JWKS.keys.find(jwk => jwk.alg === 'RSA-OAEP-256');
-    const publicKey = await jose.importJWK(jwk);
-
-    const jwe = await new jose.EncryptJWT({ 'claim': data })
-        .setProtectedHeader({ alg: 'RSA-OAEP-256', enc: 'A128CBC-HS256' })
-        .setExpirationTime('5m')
-        .encrypt(publicKey);
-
-    return jwe;
-}
-
-function readFile(fileElement) {
-    const reader = new FileReader();
-    return new Promise(resolve => {
-        reader.onload = (event) => {
-            resolve(event.currentTarget.result);
-        }
-    
-        reader.readAsBinaryString(fileElement.files[0]);
-    })
-}
-
-function signatureNonce(nonce, privateKey) {
-    const rsa = forge.pki.rsa;
-    const md = forge.md.sha256.create();
-    md.update(nonce, 'utf8');
-    const key = forge.pki.privateKeyFromPem(privateKey);
-    return btoa(key.sign(md));
-}
-
-function hasDebugParameters() {
-    const privateKey = document.querySelector('input[name="privateKey"]');
-    const publicCertificate = document.querySelector('input[name="publicCertificate"]');
-    if (privateKey.files.length === 0) return false; 
-    if (publicCertificate.files.length === 0) return false; 
-    return true;
-}
-
-async function submitWhenDebugMode(mode) {
-    if (!hasDebugParameters()) return;
-
-    const form = document.querySelector('form#kc-form-login');
-    form.mode.value = mode;
-
-    //  公開鍵をJWEで暗号化してフォームに設定
-    const publicCertificateElement = document.querySelector('#publicCertificate');
-    const publicCertificate = await readFile(publicCertificateElement);
-    const realm = document.querySelector('#realm').value;
-    const jwksPath = `/realms/${realm}/protocol/openid-connect/certs`;
-    const jwksUrl = new URL(jwksPath, location.href);
-    const jwe = await encryptDataAsJWE(publicCertificate, jwksUrl);
-    if (mode === 'login') {
-        form.encryptedUserAuthenticationCertificate.value = jwe;
-    } else {
-        form.encryptedDigitalSignatureCertificate.value = jwe;
-    }
-
-    //  nonceをフォームに設定
-    const nonce = document.querySelector('#nonce').value;
-    form.applicantData.value = nonce;
-
-    //  nonceのハッシュを秘密鍵で署名してフォームに設定
-    const privateKeyElement = document.querySelector('#privateKey');
-    const privateKey = await readFile(privateKeyElement);
-    const signature = signatureNonce(nonce, privateKey);
-    form.sign.value = signature;
-
-    form.submit();
-}
-
 function switchEnableActionRegistrationButtons() {
   if(!document.querySelector('input[name="agree-tos"]').checked ||
     !document.querySelector('input[name="agree-pp"]').checked) {
@@ -99,28 +25,6 @@ function onOpenRegistrationButton() {
     document.querySelector('input[name="registration"]').setAttribute('disabled', true);
     document.querySelector('#userLogin').style.display = 'none';
     document.querySelector('#userRegistration').style.display = 'block';
-    document.querySelector('div[name="debug-form-block"]');
-    moveToRegistrationBlockWhenDebugModeEnable();
-}
-
-function moveToRegistrationBlockWhenDebugModeEnable() {
-    let srcElement = document.querySelector('div[name="debug-form-block"]');
-    if (srcElement == null) {
-        return;
-    }
-    let dstElement = document.querySelector('#kc-form-options + div');
-    dstElement.appendChild(srcElement);
-    srcElement.classList.add("margin-top-20px");
-}
-
-function moveToLoginBlockWhenDebugModeEnable() {
-    let srcElement = document.querySelector('div[name="debug-form-block"]');
-    if (srcElement == null) {
-        return;
-    }
-    let dstElement = document.querySelector('#userLogin');
-    dstElement.insertAdjacentElement('afterbegin', srcElement);
-    srcElement.classList.remove("margin-top-20px");
 }
 
 function addEventListeners() {
@@ -131,14 +35,10 @@ function addEventListeners() {
     goBackLink.addEventListener('click', (event) => {
         document.querySelector('#userLogin').style.display = 'block';
         document.querySelector('#userRegistration').style.display = 'none';
-        moveToLoginBlockWhenDebugModeEnable();
     });
 
     let loginButton = document.querySelector('input[name="login"][type="button"]');
     loginButton.addEventListener('click', () => { onClickActionButton('login'); });
-
-    let replacementButton = document.querySelector('input[name="replacement"][type="button"]');
-    replacementButton.addEventListener('click', () => { onClickActionButton('replacement'); });
 
     let agreeTosButton = document.querySelector('input[name="agree-tos"]');
     agreeTosButton.addEventListener('input', (event) => { switchEnableActionRegistrationButtons(); });
