8599de2ca7420b618cf16b030c2bdfdaa343287d
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-09-28 00:51:21 +0000

    不要なimportを削除

--- backend/keycloak/my-numbercard-authenticator/src/test/java/com/example/mynumbercardidp/keycloak/rest/CustomAttributeProviderTest.java
+++ backend/keycloak/my-numbercard-authenticator/src/test/java/com/example/mynumbercardidp/keycloak/rest/CustomAttributeProviderTest.java
@@ -1,7 +1,6 @@
 package com.example.mynumbercardidp.keycloak.rest;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
