fd616ba717181d3ec4f1f12698190d88dcff07e6
hiromu-bit <hiromu0621@icloud.com>
2023-10-25 09:55:39 +0900

    app.pyに修正（型ヒント追加）

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+app: str
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return "Hello, World!"
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host="0.0.0.0", port=3000)
