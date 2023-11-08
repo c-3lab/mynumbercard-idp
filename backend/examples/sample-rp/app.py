872b9b2f095b1ecdba10470d197d4fd75a2f6da1
ryosuke-betw <sir.kasama1218@icloud.com>
2023-11-08 13:38:52 +0900

    連携前画面に遷移する関数

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -23,5 +23,10 @@ def connected() -> str:
     return render_template("connected.html")
 
 
+@app.route("/connect")
+def connect() -> str:
+    return render_template("connect.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
