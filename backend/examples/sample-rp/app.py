4297dedc7cb76fc2ba29dacb7ad37e04275c673c
ryosuke-betw <sir.kasama1218@icloud.com>
2023-11-21 17:55:36 +0900

    コンフリクト解消コミット３

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -18,19 +18,5 @@ def connected() -> str:
     return render_template("connected.html")
 
 
-@app.route("/connected")
-def connected() -> str:
-    return render_template("connected.html")
-
-@app.route("/connect")
-def connect() -> str:
-    return render_template("connect.html")
-
-
-@app.route("/connected")
-def connected() -> str:
-    return render_template("connected.html")
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
