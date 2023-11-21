a43c837c71a27ae3eaa11f8f3e5656d9b27b8580
ryosuke-betw <sir.kasama1218@icloud.com>
2023-11-21 17:46:38 +0900

    resolve conflict commit

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -64,6 +64,10 @@ def auth() -> str:
 
     return redirect(url_for("index"))
 
+@app.route("/connect")
+def connect() -> str:
+    return render_template("connect.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
