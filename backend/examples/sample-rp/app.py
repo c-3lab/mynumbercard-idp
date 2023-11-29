a5ff4c8238fa3aeb9454aeaddf03187522c1e8bc
ryosuke-betw <sir.kasama1218@icloud.com>
2023-11-29 13:21:07 +0900

    コードの整形

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -3,11 +3,11 @@ from authlib.integrations.flask_client import OAuth
 import os
 
 
-#config
+# config
 app: Flask = Flask(__name__)
-app.secret_key=os.getenv("APP_SECRET_KEY")
+app.secret_key = os.getenv("APP_SECRET_KEY")
 
-app.config.update(    
+app.config.update(
     OIDC_ID_TOKEN_COOKIE_SECURE=False,
     OIDC_USER_INFO_ENABLED=True,
     SERVICE_ID=os.getenv("SERVICE_ID"),
@@ -17,25 +17,25 @@ app.config.update(
 
 oauth: OAuth = OAuth(app)
 oauth.register(
-    name='keycloak',
+    name="keycloak",
     client_id=os.getenv("KEYCLOAK_CLIENT_ID"),
     client_secret=os.getenv("KEYCLOAK_CLIENT_SECRET"),
     authorize_url=f'{os.getenv("KEYCLOAK_URL")}/realms/{os.getenv("KEYCLOAK_REALM")}/protocol/openid-connect/auth',
-    authorize_params=None,    
+    authorize_params=None,
     server_metadata_url=f'{os.getenv("KEYCLOAK_URL")}/realms/{os.getenv("KEYCLOAK_REALM")}/.well-known/openid-configuration',
     access_token_url=f'{os.getenv("KEYCLOAK_URL")}/realms/{os.getenv("KEYCLOAK_REALM")}/protocol/openid-connect/token',
-    access_token_params=None,   
-    api_base_url=f'{os.getenv("BASE_URL")}', 
+    access_token_params=None,
+    api_base_url=f'{os.getenv("BASE_URL")}',
     client_kwargs={
         "scope": "openid",
-    }
+    },
 )
 
 
 @app.route("/")
 def index() -> str:
-    user: object = session.get('user')
-    return render_template("index.html",user=user)
+    user: object = session.get("user")
+    return render_template("index.html", user=user)
 
 
 @app.route("/login")
@@ -47,22 +47,22 @@ def login() -> str:
 def connected() -> str:
     return render_template("connected.html")
 
-  
-@app.route('/Keycloak-login')
+
+@app.route("/Keycloak-login")
 def loginKeycloak() -> str:
-    redirect_uri: str = url_for('auth', _external=True)
+    redirect_uri: str = url_for("auth", _external=True)
     return oauth.keycloak.authorize_redirect(redirect_uri)
 
 
-@app.route('/auth')
+@app.route("/auth")
 def auth() -> str:
     token: OAuth = oauth.keycloak.authorize_access_token()
-    userinfo: OAuth = token['userinfo']
+    userinfo: OAuth = token["userinfo"]
     if userinfo:
-        session['user'] = userinfo
-        session['token'] = token
+        session["user"] = userinfo
+        session["token"] = token
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
