4b38254b14ad6820843e34456354116155276d77
ryosuke-betw <sir.kasama1218@icloud.com>
2023-11-29 13:11:35 +0900

    型ヒントを追加

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, redirect, url_for, session,request
+from flask import Flask, render_template, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 import os
 
 
 #config
 app: Flask = Flask(__name__)
-app.secret_key = os.getenv("APP_SECRET_KEY")
+app.secret_key=os.getenv("APP_SECRET_KEY")
 
 app.config.update(    
     OIDC_ID_TOKEN_COOKIE_SECURE=False,
@@ -34,7 +34,7 @@ oauth.register(
 
 @app.route("/")
 def index() -> str:
-    user = session.get('user')
+    user: object = session.get('user')
     return render_template("index.html",user=user)
 
 
@@ -49,15 +49,15 @@ def connected() -> str:
 
   
 @app.route('/Keycloak-login')
-def loginKeycloak():
-    redirect_uri = url_for('auth', _external=True)
+def loginKeycloak() -> str:
+    redirect_uri: str = url_for('auth', _external=True)
     return oauth.keycloak.authorize_redirect(redirect_uri)
 
 
 @app.route('/auth')
-def auth():
-    token = oauth.keycloak.authorize_access_token()
-    userinfo = token['userinfo']
+def auth() -> str:
+    token: OAuth = oauth.keycloak.authorize_access_token()
+    userinfo: OAuth = token['userinfo']
     if userinfo:
         session['user'] = userinfo
         session['token'] = token
