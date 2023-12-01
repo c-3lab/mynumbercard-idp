dacef29a6f38bb9bdfdbd1bc14a1f81225008a52
ryosuke-betw <sir.kasama1218@icloud.com>
2023-12-01 13:28:44 +0900

    利用していないインポートを削除

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session,request
+from flask import Flask, render_template, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 import os
 
