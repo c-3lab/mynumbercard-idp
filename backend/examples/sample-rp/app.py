c65bd9739c67f8eb0e9e5aa03a292f01412472bc
ryosuke-betw <sir.kasama1218@icloud.com>
2023-12-01 13:13:24 +0900

    rebaseでコンフリクトを解消したコミット

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -31,107 +31,6 @@ oauth.register(
     },
 )
 
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-)
-
-#config
-app: Flask = Flask(__name__)
-app.secret_key = 'your_random_secret_key_here'
-logging.basicConfig(level=logging.DEBUG)
-app.config.update(    
-    OIDC_ID_TOKEN_COOKIE_SECURE=False,
-    OIDC_USER_INFO_ENABLED=True,
-    SERVICE_ID=os.getenv("SERVICE_ID"),
-    NOTE=os.getenv("NOTE"),
-)
-
-oauth: OAuth = OAuth(app)
-oauth.register(
-    name='keycloak',
-    client_id=os.getenv("KEYCLOAK_CLIENT_ID"),
-    client_secret=os.getenv("KEYCLOAK_CLIENT_SECRET"),
-    server_metadata_url=f'{os.getenv("KEYCLOAK_URL")}/realms/{os.getenv("KEYCLOAK_REALM")}',
-    authorize_url=f'{os.getenv("KEYCLOAK_URL")}/realms/{os.getenv("KEYCLOAK_REALM")}/protocol/openid-connect/auth',
-    client_kwargs={
-        "scope": "openid",
-    },
-)
-
-print(oauth)
-
-
-def getUser(request):
-    # RP側に個人情報を格納する変数を定義
-    idTokenContent = {}
-    
-    username = ""
-    name = ""
-    address = ""
-    gender = ""
-    dateOfBirth = ""
-    sub = ""
-    nickname = ""
-    uniqueId = ""
-    accessToken = ""
-
-    # 認証を確認し、個人情報を対応する変数に格納する
-    if accessToken != "":
-        
-        idTokenContent = req.oidc.id_token_claims
-
-        username = req.oidc.user.preferred_username
-        name = req.oidc.user.name
-        address = req.oidc.user.user_address
-        gender = req.oidc.user.gender_code
-        dateOfBirth = req.oidc.user.birth_date
-        sub = req.oidc.user.sub
-        uniqueId = req.oidc.user.unique_id
-
-        accessToken = req.oidc.access_token.access_token
-        
-        app.logger.debug('This is a debug message')
-        app.logger.info('This is an info message')
-        app.logger.warning('This is a warning message')
-        app.logger.error('This is an error message')
-        app.logger.critical('This is a critical message')
-
-        print("接続済み",flush=True)
-    else:
-        app.logger.debug('This is a debug message')
-        app.logger.info('This is an info message')
-        app.logger.warning('This is a warning message')
-        app.logger.error('This is an error message')
-        app.logger.critical('This is a critical message')
-
-        print("未接続",flush=True)
-
-    user={
-        "id_token_content": idTokenContent,
-        "username": username,
-        "name": name,
-        "address": address,
-        "gender": gender,
-        "date_of_birth": dateOfBirth,
-        "sub": sub,
-        "unique_id": uniqueId,
-        "access_token": accessToken
-    }
-
-    return user
-
 
 @app.route("/")
 def index() -> str:
@@ -169,20 +68,6 @@ def auth() -> str:
         session["token"] = token
 
     return redirect(url_for("index"))
-
-@app.route("/connect")
-def connect() -> str:
-    return render_template("connect.html")
-
-
-@app.route("/connected")
-def connected() -> str:
-    return render_template("connected.html")
-
-
-@app.route("/connected")
-def connected() -> str:
-    return render_template("connected.html")
 
 
 if __name__ == "__main__":
