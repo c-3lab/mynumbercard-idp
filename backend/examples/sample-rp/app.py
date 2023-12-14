347ad60d7a26b39a66f6f53948d9019638079ccd
ryosuke-betw <sir.kasama1218@icloud.com>
2023-12-14 13:28:06 +0900

    fixed along Ruff

--- backend/examples/sample-rp/app.py
+++ backend/examples/sample-rp/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, session
-from authlib.integrations.flask_client import OAuth
 import os
 
+from authlib.integrations.flask_client import OAuth
+from flask import Flask, Response, redirect, render_template, session, url_for
 
 # config
 app: Flask = Flask(__name__)
@@ -21,9 +21,9 @@ oauth.register(
     client_id=os.getenv("KEYCLOAK_CLIENT_ID"),
     client_secret=os.getenv("KEYCLOAK_CLIENT_SECRET"),
     authorize_url=f'{os.getenv("KEYCLOAK_URL")}/realms/{os.getenv("KEYCLOAK_REALM")}/protocol/openid-connect/auth',
-    authorize_params=None,
     server_metadata_url=f'{os.getenv("KEYCLOAK_URL")}/realms/{os.getenv("KEYCLOAK_REALM")}/.well-known/openid-configuration',
     access_token_url=f'{os.getenv("KEYCLOAK_URL")}/realms/{os.getenv("KEYCLOAK_REALM")}/protocol/openid-connect/token',
+    authorize_params=None,
     access_token_params=None,
     api_base_url=f'{os.getenv("BASE_URL")}',
     client_kwargs={
@@ -54,7 +54,7 @@ def connected() -> str:
 
 
 @app.route("/Keycloak-login")
-def loginKeycloak() -> str:
+def login_keycloak() -> str:
     redirect_uri: str = url_for("auth", _external=True)
     return oauth.keycloak.authorize_redirect(redirect_uri)
 
@@ -71,4 +71,4 @@ def auth() -> str:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run(debug=True, host="0.0.0.0", port=3000)  # noqa: S104
