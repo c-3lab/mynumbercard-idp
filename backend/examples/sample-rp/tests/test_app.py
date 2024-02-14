d8f2546c6d4c496ff7e6b43f571d03e9f6454a3f
ryosuke-betw <sir.kasama1218@icloud.com>
2024-02-14 15:10:59 +0900

    add tests of response status code

--- backend/examples/sample-rp/tests/test_app.py
+++ backend/examples/sample-rp/tests/test_app.py
@@ -15,42 +15,47 @@ def client():
 
 def test_index_without_user(client):
     response = client.get("/")
+    assert response.status_code == 200
     assert "新規登録・ログイン".encode() in response.data
 
 
 def test_index_with_user(client):
     with client.session_transaction() as sess:
-        sess['user'] = {'username': 'test_user'}
+        sess["user"] = {"username": "test_user"}
 
     response = client.get('/')
     assert response.status_code == 200
-    assert b'<title>SampleRP Website</title>' in response.data
+    assert b"<title>SampleRP Website</title>" in response.data
     assert "アカウント情報照会".encode() in response.data
-
 
 
 def test_login(client):
     response = client.get("/login")
+    assert response.status_code == 200
     assert "<title>新規会員登録/ログイン</title>".encode() in response.data
 
 
 def test_connect(client):
     response = client.get("/connect")
+    assert response.status_code == 200
     assert "<title>連携前画面</title>".encode() in response.data
 
 
 def test_connected(client):
     response = client.get("/connected")
+    assert response.status_code == 200
     assert "<title>連携後</title>".encode() in response.data
 
 
 def test_account(client):
     response = client.get("/account")
+    assert response.status_code == 200
     assert "<title>アカウント情報照会</title>".encode() in response.data
 
 
 def test_token(client):
     response = client.get("/token")
+    assert response.status_code == 200
     assert "<title>ID/トークン情報</title>".encode() in response.data
 
 
